63b0c0dd5b4d8019323ba65e0c67f9c8f9ff602f
caerulescens <caerulescens.github@proton.me>
2023-11-19 16:43:41 -0500

    refactor: remove argocd + helm provider

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.23.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.11.0"
-    }
     local = {
       source  = "hashicorp/local"
       version = ">=2.4.0"
