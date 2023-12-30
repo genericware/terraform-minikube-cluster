913ff7b435b25e5b682304d8fa88b9d468891d8d
caerulescens <caerulescens.github@proton.me>
2023-12-30 12:53:30 -0500

    fix: remove local provider

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.23.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = ">=2.4.0"
-    }
     minikube = {
       source  = "scott-the-programmer/minikube"
       version = "0.3.2"
